93e273e9e99c2cccf881db326f84772544f7aec6
liuguojunjp <liuguojunjp@gmail.com>
2020-09-18 21:26:14 +0900

    20200907

---
+++ 20200906/src/main/kotlin/com/liu/study/p20200906/main.kt
@@ -0,0 +1,5 @@
+package com.liu.study.p20200906
+
+fun main() {
+    println("hello liu birsday!!!")
+}
---
+++ 20200907/src/main/kotlin/kotlin/com/liu/study/p20200906/main.kt
@@ -0,0 +1,5 @@
+package com.liu.study.p20200907
+
+fun main() {
+    println("hello liu birsday!!!")
+}
